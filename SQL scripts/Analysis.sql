7c726ecaad5da5b7c8dba04e354a9f92653ff93a
Rosh00L <rosh00lg@gmail.com>
2025-03-04 21:34:20 +0000

    update

---
+++ SQL scripts/Analysis.sql
@@ -0,0 +1,90 @@
+/* Generic Question
+1.How many unique cities does the data have? */
+select
+distinct city,
+Branch
+from sales; 
+
+
+/* Product Analysis
+-- 1. How many unique product lines does each city and branch have?*/
+select
+city,
+Branch,
+count(distinct `Product line`)
+from sales
+group by city,Branch
+; 
+
+-- 2. What is the most common payment method?
+select
+Payment,
+count(Payment) as Payment_cnt
+from sales
+group by Payment
+order by Payment_cnt desc
+;
+
+-- 3. What is the most selling product line?
+select 
+`Product line`,
+sum(`Quantity`) as qty 
+from sales
+group by `Product line`
+order by qty desc
+;
+
+-- 4. What is the total revenue by month?
+
+select
+month_name as month,
+sum(total) as Revenue 
+from sales
+group by month_name
+order by Revenue desc
+; 
+
+-- 5. What month had the largest COGS?
+
+select
+month_name as month,
+sum(cogs) as cogs_sum
+from sales
+group by month_name
+order by cogs_sum desc
+;
+
+-- 6. What product line had the largest revenue?
+select
+`Product line`,
+sum(total) as Revenue
+from sales
+group by `Product line`
+order by Revenue desc
+; 
+
+-- 7. What is the city with the largest revenue?
+
+select
+city,
+sum(total) as Revenue
+from sales
+group by city
+order by Revenue desc
+; 
+
+-- 8. What product line had the largest VAT?
+
+select
+`Product line`,
+sum(`Tax 5%`) as VAT_sum
+from sales
+group by `Product line`
+order by VAT_sum desc
+; 
+
+-- 10. Which branch sold more products than average product sold?
+
+-- 11. What is the most common product line by gender?
+
+-- 12. What is the average rating of each product line?
--- SQL scripts/Tables.sql
+++ SQL scripts/Tables.sql
@@ -1,6 +1,35 @@
-alter table sales add column Date_ Varchar(10);
+use salesdata;
+
+alter table sales 
+add column Date_ Varchar(10),
+add column Time_ Varchar(10),
+add column month_name varchar(15),
+add column day_name varchar(10),
+add column Day_time varchar(15);
+
+
 update sales
-set Date_=str_to_date(date,'%d/%m/%Y');
+set
+
+Date_=str_to_date(Date,'%d/%m/%Y'),
+Time_=time_format(Time,"%T"),
+month_name= monthname(date_),
+day_name= dayname(date_),
+Day_time=(
+case 
+when Time_ between "00:00:00" and "12:00:00" then "Morning" 
+when Time_ between "12:00:00" and "16:00:00" then "Afternoon"
+else "Evening" 
+end
+);
+
 
 alter table sales
-	drop date;
+	drop Date,
+    drop Time
+    ;
+    
+alter table sales
+	Rename  column Date_ to Date, 
+    Rename  column Time_ to Time;
+   
